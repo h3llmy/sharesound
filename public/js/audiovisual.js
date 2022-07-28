021fdb52339fa2c9f04acf438f7daacbf1e9736c
h3llmy <helmytrisna86@gmail.com>
2022-07-28 13:24:38 +0800

    fix Equalizer

--- public/js/audiovisual.js
+++ public/js/audiovisual.js
@@ -19,12 +19,6 @@ cursorColor: '#000',
 cursorWidth : 0.1,
 });
 
-// audio load
-wavesurfer.load('audio/H3llmy - DImention Of Taka.mp3');
-// wavesurfer.load('audio/Laur - Animosity [From WACCA] MV.mp3');
-// wavesurfer.load('audio/Laur - Metamorphose [From WACCA].mp3');
-// Laur - Metamorphose [From WACCA].mp3
-
 // audio play and pauses
 btnPlay.onclick = function(){
     wavesurfer.playPause();
@@ -35,20 +29,20 @@ btnPlay.onclick = function(){
     }
 }
 
-window.addEventListener('keydown', (e) => {  
-    if (e.keyCode === 32 && e.target === document.body) {  
-      e.preventDefault();  
+window.addEventListener('keydown', (e) => {
+    if (e.keyCode === 32 && e.target === document.body) {
+      e.preventDefault();
       wavesurfer.playPause();
       if(btnPlay.src.includes("play.png")){
           btnPlay.src = "img/pause.png"
       }else{
           btnPlay.src = "img/play.png"
       }
-    }  
+    }
   });
 
 // audio volume
-volumeSlider.addEventListener("mousemove", () => {
+volumeSlider.addEventListener("input", () => {
     changeVolume(volumeSlider.value);
 });
 var changeVolume = (volume) => {
@@ -69,13 +63,13 @@ btnStop.onclick = function(){
     timeCurrent.innerText = '0:00'
 }
 
-window.addEventListener('keydown', (e) => {  
-    if (e.key === 's' && e.target === document.body) {  
-      e.preventDefault();  
+window.addEventListener('keydown', (e) => {
+    if (e.key === 's' && e.target === document.body) {
+      e.preventDefault();
       wavesurfer.stop();
       btnPlay.src = "img/play.png"
       timeCurrent.innerText = '0:00'
-    }  
+    }
   });
 
 // audio mute
@@ -93,9 +87,9 @@ btnVolume.onclick = function(){
     }
 }
 
-window.addEventListener('keydown', (e) => {  
-    if (e.key === 'm' && e.target === document.body) {  
-      e.preventDefault();  
+window.addEventListener('keydown', (e) => {
+    if (e.key === 'm' && e.target === document.body) {
+      e.preventDefault();
         if(btnVolume.src.includes("volume.png")){
             wavesurfer.setMute(true);
             btnVolume.src = "img/mute.png"
@@ -107,7 +101,7 @@ window.addEventListener('keydown', (e) => {
             wavesurfer.setMute(false);
             btnVolume.src = "img/volume.png"
         }
-    }  
+    }
   });
 
 // track audio
@@ -143,81 +137,82 @@ wavesurfer.on('ready', function () {
 });
 
 // Equalizer
-wavesurfer.on('ready', function () {
-    var EQ = [
-      {
-        f: 32,
-        type: 'lowshelf'
-      }, {
-        f: 64,
-        type: 'peaking'
-      }, {
-        f: 125,
-        type: 'peaking'
-      }, {
-        f: 250,
-        type: 'peaking'
-      }, {
-        f: 500,
-        type: 'peaking'
-      }, {
-        f: 1000,
-        type: 'peaking'
-      }, {
-        f: 2000,
-        type: 'peaking'
-      }, {
-        f: 4000,
-        type: 'peaking'
-      }, {
-        f: 8000,
-        type: 'peaking'
-      }, {
-        f: 16000,
-        type: 'highshelf'
-      }
-    ];
-  
-    // Create filters
-    var filters = EQ.map(function (band) {
-      var filter = wavesurfer.backend.ac.createBiquadFilter();
-      filter.type = band.type;
-      filter.gain.value = 0;
-      filter.Q.value = 1;
-      filter.frequency.value = band.f;
-      return filter;
-    });
-  
-    // Connect filters to wavesurfer
-    wavesurfer.backend.setFilters(filters);
-  
-    // Bind filters to vertical range sliders
-    var container = document.querySelector('#equalizer');
-    filters.forEach(function (filter) {
-      var input = document.createElement('input');
-      wavesurfer.util.extend(input, {
-        type: 'range',
-        min: -20,
-        max: 20,
-        value: 0,
-        title: filter.frequency.value
-      });
-      input.style.display = 'inline-block';
-      input.setAttribute('orient', 'vertical');
-      wavesurfer.drawer.style(input, {
-        'webkitAppearance': 'slider-vertical',
-        width: '50px',
-        height: '150px'
-      });
-      container.appendChild(input);
-  
-      var onChange = function (e) {
-        filter.gain.value = ~~e.target.value;
-      };
-  
-      input.addEventListener('input', onChange);
-      input.addEventListener('change', onChange);
-    });
-  
-    wavesurfer.filters = filters;
+  var EQ = [
+    {
+      f: 32,
+      type: 'lowshelf'
+    }, {
+      f: 64,
+      type: 'peaking'
+    }, {
+      f: 125,
+      type: 'peaking'
+    }, {
+      f: 250,
+      type: 'peaking'
+    }, {
+      f: 500,
+      type: 'peaking'
+    }, {
+      f: 1000,
+      type: 'peaking'
+    }, {
+      f: 2000,
+      type: 'peaking'
+    }, {
+      f: 4000,
+      type: 'peaking'
+    }, {
+      f: 8000,
+      type: 'peaking'
+    }, {
+      f: 16000,
+      type: 'highshelf'
+    }
+  ];
+
+  // Create filters
+  var filters = EQ.map(function (band) {
+    var filter = wavesurfer.backend.ac.createBiquadFilter();
+    filter.type = band.type;
+    filter.gain.value = 0;
+    filter.Q.value = 1;
+    filter.frequency.value = band.f;
+    return filter;
   });
+
+  // Connect filters to wavesurfer
+  wavesurfer.backend.setFilters(filters);
+
+  // Bind filters to vertical range sliders
+  var container = document.querySelector('#equalizer');
+  filters.forEach(function (filter) {
+    var input = document.createElement('input');
+    wavesurfer.util.extend(input, {
+      type: 'range',
+      min: -20,
+      max: 20,
+      value: 0,
+      title: filter.frequency.value
+    });
+    input.style.display = 'inline-block';
+    input.setAttribute('orient', 'vertical');
+    wavesurfer.drawer.style(input, {
+      'webkitAppearance': 'slider-vertical',
+      width: '50px',
+      height: '150px'
+    });
+  //   container.innerHTML = "";
+    container.appendChild(input);
+
+  //   console.log(input);
+
+    var onChange = function (e) {
+      filter.gain.value = ~~e.target.value;
+    };
+
+    input.addEventListener('input', onChange);
+    input.addEventListener('change', onChange);
+  });
+
+  wavesurfer.filters = filters;
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,19 @@
                     <div id="waveform">
                     </div>
                 </div>
-            </div>      
+            </div>
+            <div hidden>
+            <audio src="" controls id="audioPlayer">
+                Sorry, your browser doesn't support html5!
+            </audio>
+            </div>
+            <ul id="playlist">
+
+                <li class="current-song">
+                    <a onclick="wavesurfer.load('audio/H3llmy - DImention Of Taka.mp3')">Exit the Premises</a></li>
+                <li><a onclick="wavesurfer.load('audio/Laur - Animosity [From WACCA] MV.mp3')">Severe Tire Damage</a></li>
+                <li><a onclick="wavesurfer.load('audio/Laur - Metamorphose [From WACCA].mp3')">Broken Reality</a></li>
+            </ul>
         <div>
     </div>
 </div>
